a80339df808b1c582effac537aa83d679675b6a5
McChakLeung <441337516@qq.com>
2019-08-13 17:46:03 +0800

    许可树展示 -- 数据库交互：一次性查询所有数据再进行处理（Demo4）

--- atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/manager/controller/PermissionController.java
+++ atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/manager/controller/PermissionController.java
@@ -24,7 +24,17 @@ public class PermissionController {
     }
 
     /**
-     * Demo3 -- 数据库交互：实现三级菜单加载
+     * Demo3 -- 数据库交互：递归调用
+     *
+     * 递归：
+     * 1.基准条件
+     * 2.范围逐渐缩小
+     * 3.调用自身函数
+     *
+     * 缺点：
+     * 1.多次查询数据库，数据库压力大
+     * 2.递归调用会在内存栈区压栈，容易造成内存溢出
+     *
      * @return
      */
     @ResponseBody
@@ -42,25 +52,7 @@ public class PermissionController {
             root.add(parent);
 
             //设置一个childrenList集合，用于接收从数据库中查询的子节点
-            List<Permission> childrenList = permissionService.queryPermissionByParenetID(parent.getId());
-            parent.setChildren(childrenList);
-
-            //遍历子节点(foreach循环）
-            for (Permission children: childrenList) {
-                List<Permission> sonList = permissionService.queryPermissionByParenetID(children.getId());
-                children.setChildren(sonList);
-            }
-
-//            //遍历子节点
-//            Permission child1 = new Permission();
-//            child1.setName("子节点1");
-//            Permission child2 = new Permission();
-//            child2.setName("子节点2");
-//
-//            //设置关联关系
-//            childrenList.add(child1);
-//            childrenList.add(child2);
-//            parent.setChildren(childrenList);
+            queryChildrenPerminssion(parent);
 
             result.setDatas(root);
             result.setSuccess(true);
@@ -71,6 +63,54 @@ public class PermissionController {
         }
         return result;
     }
+
+    private void queryChildrenPerminssion(Permission parent){
+        List<Permission> childrenList = permissionService.queryPermissionByParenetID(parent.getId());
+        parent.setChildren(childrenList);
+
+        //遍历子节点(foreach循环）
+        for (Permission children: childrenList) {
+            queryChildrenPerminssion(children);
+        }
+    }
+
+//    /**
+//     * Demo2-- 数据库交互：实现三级菜单加载
+//     * @return
+//     */
+//    @ResponseBody
+//    @RequestMapping("/loadData")
+//    public Object loadData(){
+//
+//        AjaxResult result = new AjaxResult();
+//
+//        try{
+//            //设置父节点list
+//            List<Permission> root = new ArrayList();
+//
+//            //查询数据库父节点
+//            Permission parent = permissionService.queryParentPermission();
+//            root.add(parent);
+//
+//            //设置一个childrenList集合，用于接收从数据库中查询的子节点
+//            List<Permission> childrenList = permissionService.queryPermissionByParenetID(parent.getId());
+//            parent.setChildren(childrenList);
+//
+//            //遍历子节点(foreach循环）
+//            for (Permission children: childrenList) {
+//                List<Permission> sonList = permissionService.queryPermissionByParenetID(children.getId());
+//                children.setChildren(sonList);
+//            }
+//
+//            result.setDatas(root);
+//            result.setSuccess(true);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            result.setSuccess(false);
+//            result.setMessage("加载许可树失败");
+//        }
+//        return result;
+//    }
 
     //Demo1 -- 无数据库交互（模拟实现）
 //    @ResponseBody
