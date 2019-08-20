360026a151953cdcd9ae61e940d99570e67d8b33
McChakLeung <441337516@qq.com>
2019-08-20 23:02:52 +0800

    角色维护显示数据调bug

--- atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/manager/service/impl/RoleServiceImpl.java
+++ atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/manager/service/impl/RoleServiceImpl.java
@@ -19,12 +19,15 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public Page<Role> queryRoleListByParams(Map<String, Object> params) {
 
-        List<Role> roleList = roleMapper.queryRoleListByParams(params);
-
+        //初始化Page对象
         Page<Role> page = new Page((Integer) params.get("pageno"),(Integer)params.get("pagesize"));
-
+        //根据初始化Page类对象获得起始行的值
+        Integer startline = page.getStartline();
+        //将startline存入到参数集合中
+        params.put("startline",startline);
+        //将数据库中查询出来的Role数据查询出来并存放到page对象中，将page对象返回给控制器
+        List<Role> roleList = roleMapper.queryRoleListByParams(params);
         page.setDatalist(roleList);
-
         return page;
     }
 }
